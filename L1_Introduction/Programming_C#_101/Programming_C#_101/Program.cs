f3d9702450220a193566b81788353b848a68ceed
Phat Luong <phat.x.luong@gmail.com>
2024-04-16 22:09:00 +0700

    1st commit

--- L1_Introduction/Programming_C#_101/Programming_C#_101/Program.cs
+++ L1_Introduction/Programming_C#_101/Programming_C#_101/Program.cs
@@ -1,11 +1,20 @@
 ﻿//Part 1 Print a string to the console
 //hint: Console.WriteLine("Hello World!");
+Console.WriteLine("How old are you?");
 
 //Part 2 Get user input and print it to the console
 //hint: Console.ReadLine();
+string userAge = Console.ReadLine();
+Console.WriteLine($"Your age is: {userAge}\n****************\n next question:");
 
 //Part 3 Get two inputs from the user and print them on separate lines
 //hint: use \n to create a new line
+Console.WriteLine("What is first name?");
+string firstName = Console.ReadLine();
+Console.WriteLine("What is last name?");
+string lastName = Console.ReadLine();
+Console.WriteLine($"Your first name is: {firstName}\nYour last name is {lastName}");
+
 
 
 
