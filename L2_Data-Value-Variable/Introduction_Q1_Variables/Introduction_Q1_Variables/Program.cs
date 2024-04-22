e3368d0863ced41739ef5904f0c35574f70ed74d
Phat X Luong <66936482+phatx88@users.noreply.github.com>
2024-04-22 11:26:35 +0700

    add answer

--- L2_Data-Value-Variable/Introduction_Q1_Variables/Introduction_Q1_Variables/Program.cs
+++ L2_Data-Value-Variable/Introduction_Q1_Variables/Introduction_Q1_Variables/Program.cs
@@ -14,43 +14,64 @@
 // int variable
 // Create an integer variable named 'enemy_health' and assign it the value 100.
 // This variable type is used to store whole numbers.
+int enemy_health = 100;
+Console.WriteLine(enemy_health);
 
 // double variable
 // Create a double variable named 'game_currency' and assign it the value 2500.50.
 // This variable type is used to store decimal numbers.
+double game_currency = 2500.50;
+Console.WriteLine(game_currency);
 
 // string variable
 // Create a string variable named 'player_name' and assign it the value "John Doe".
 // This variable type is used to store text.
+string player_name = "John Doe";
+Console.WriteLine(player_name);
 
 // bool variable
 // Create a boolean variable named 'isalive' and assign it the value true.
 // This variable type is used to store true or false values.
+bool isalive = true;
+if (isalive) Console.WriteLine("is Alive");
+else Console.WriteLine("is Dead");
 
-// char variable
-// Create a character variable named 'patrol_state' and assign it the value 'S' for "Standby".
-// This variable type is used to store single characters.
+    // char variable
+    // Create a character variable named 'patrol_state' and assign it the value 'S' for "Standby".
+    // This variable type is used to store single characters.
+char patrol_state = 'S';
+Console.WriteLine(patrol_state);
 
 // Byte variable
 // Create a byte variable named 'player_age' and assign it the value 25.
 // This variable type is used to store whole numbers from 0 to 255.
+byte player_age = 25;
+Console.WriteLine(player_age);
 
 // Long variable
 // Create a long variable named 'score' and assign it the value 1000000000.
 // This variable type is used to store whole numbers.
+long score = 1000000000;
+Console.WriteLine(score);
 
 // Decimal variable
 // Create a decimal variable named 'price' and assign it the value 100.50.
 // This variable type is used to store decimal numbers.
+decimal price = 100.50m;
+Console.WriteLine(price);
 
 // Null variable
 // Create a variable named 'null_variable' and assign it the value null.
 // This variable type is used to store no value.
+var null_variable = 0;
+
 
 // Float variable
 // Create a float variable named 'speed' and assign it the value 100.50.
 // This variable type is used to store decimal numbers.
 //---------------------------------------------------------------------
+float speed = 100.50;
+Console.WriteLine(speed);
 
 // Part 17: Declaration of Constants
 // Write a C# program to declare a constant integer variable representing the value of Pi.
